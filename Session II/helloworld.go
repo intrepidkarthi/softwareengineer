2ef23d0e795feaf24ea9d53b04158aea0a3902e2
Karthikeyan NG <kng@nference.net>
2021-10-09 12:39:19 +0530

    Adding Session III contents

---
+++ Session II/helloworld.go
@@ -0,0 +1,14 @@
+package main
+
+
+import "fmt"
+import "rsc.io/quote"
+
+// func main() {
+//     fmt.Println("Hello, World!")
+// 	fmt.Println("quote.Go()")
+// }
+
+func main() {
+    fmt.Println(quote.Go())
+}
